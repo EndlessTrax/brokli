3fde5e27bb5ff26d897200109aab0e60c1d436c5
Ricky White <ricky@rickywhite.net>
2025-09-20 14:02:31 -0400

    Updated `check` command to use new code.

--- cmd/check.go
+++ cmd/check.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/spf13/cobra"
 
@@ -19,9 +20,7 @@ var checkCmd = &cobra.Command{
 	Short: "TODO: Add a short description here",
 	Long:  `TODO: Add a longer description here`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Take the first argument and use it as the URL
-		r := p.FindAllLinks(p.ParseHTML(p.GetHTML(args[0])))
-		fmt.Println(r)
+		cmd.Help()
 	},
 }
 
@@ -30,7 +29,36 @@ var checkUrlCmd = &cobra.Command{
 	Short: "Check a URL",
 	Long:  `Check a URL`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Check URL command ran")
+		// Take the first argument and use it as the URL
+		urlStr := args[0]
+
+		// Parse the URL to get a base URL for resolution
+		baseUrl, err := url.Parse(urlStr)
+		if err != nil {
+			fmt.Printf("Error parsing URL: %v\n", err)
+			return
+		}
+
+		// Get HTML content
+		htmlBytes, err := p.GetHTML(urlStr)
+		if err != nil {
+			fmt.Printf("Error fetching HTML: %v\n", err)
+			return
+		}
+
+		// Parse HTML into document tree
+		doc, err := p.ParseHTML(htmlBytes)
+		if err != nil {
+			fmt.Printf("Error parsing HTML: %v\n", err)
+			return
+		}
+
+		// Get page results with all links
+		results := p.GetPageResults(doc, *baseUrl)
+		fmt.Printf("Found %d links:\n", len(results.Links))
+		for i, link := range results.Links {
+			fmt.Printf("%d. %s -> %s\n", i+1, link.Text, link.AbsoluteUrl.String())
+		}
 	},
 }
 
